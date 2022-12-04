5e8f0043a0bf085912ca2276c98c89b9baaed0e7
Anton Vlasenko <anton.vlasenko94@gmail.com>
2022-12-04 15:56:15 +0000

    Final commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,8 +3,38 @@ import { fetchUserData, cancelFetch } from '../modules/dataFetcher';
 import { Userlist } from './Userlist';
 
 export class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { userData: null };
+  };
+  
+  loadUserData() {
+    this.setState({ userData: null });
+    this.fetchID = fetchUserData(this.props.username, (userData) => {
+      this.setState({ userData });
+    });
+  };
+
+  componentDidMount() {
+    this.loadUserData();
+  };
+
+  componentWillUnmount() {
+    cancelFetch(this.fetchID);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.username !== prevProps.username) {
+      cancelFetch(this.fetchID);
+      this.loadUserData();
+    }
+  }
+
   render() {
-    const isLoading = true;
+    const isLoading = this.state.userData === null ? true : false;
+    const name = isLoading ? 'Loading...' : this.state.userData.name;
+    const bio = isLoading ? 'Bio goes here' : this.state.userData.bio;
+    const friends = isLoading ? [] : this.state.userData.friends;
 
     let className = 'Profile';
     if (isLoading) {
@@ -13,13 +43,17 @@ export class Profile extends React.Component {
 
     return (
       <div className={className}>
-        <div className="profile-picture"></div>
+        <div className="profile-picture">
+          {!isLoading && (
+            <img src={this.state.userData.profilePictureUrl} alt="" />
+          )}
+        </div>
         <div className="profile-body">
-          <h2>Name goes here</h2>
+          <h2>{name}</h2>
           <h3>@{this.props.username}</h3>
-          <p>Bio goes here</p>
+          <p>{bio}</p>
           <h3>My friends</h3>
-          <Userlist usernames={[]} onChoose={this.props.onChoose} />
+          <Userlist usernames={friends} onChoose={this.props.onChoose} />
         </div>
       </div>
     );
